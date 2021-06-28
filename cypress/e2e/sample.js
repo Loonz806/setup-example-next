fc53c18ecb796f4f4920597340abeabcbd177ab0
Lenny Peters <lpeters3@lululemon.com>
2021-06-28 09:09:24 -0700

    test: 💍 added some extra comments and resources to the example
    test in the e2e suite

--- cypress/e2e/sample.js
+++ cypress/e2e/sample.js
@@ -2,6 +2,7 @@ import "cypress-axe";
 
 describe("the setup-example page", () => {
   // Now using Cypress/Testing-Library :)
+  // https://testing-library.com/docs/cypress-testing-library/intro/ Extends cy so it uses some RTL syntax
   beforeEach(() => {
     // Load our app before starting each test case
     cy.visit("/");
@@ -16,7 +17,22 @@ describe("the setup-example page", () => {
 
   it("loads the application", () => {
     cy.get("#__next").should("exist");
+    // Visual regression snapshots are uploaded to BrowserStacks Percy which does dom change diffing
     cy.percySnapshot("Page loaded");
+    cy.checkA11y();
+  });
+
+  it("should find a headline with Hello World", () => {
+    cy.findByRole("heading", { name: /hello world/i }).should("exist");
+    cy.percySnapshot("finds the headline with Hello World");
+    cy.checkA11y();
+  });
+
+  it("should find my example component", () => {
+    cy.findByRole("heading", { name: /i am an example component/i }).should(
+      "exist"
+    );
+    cy.percySnapshot("finds the example component");
     cy.checkA11y();
   });
 });
