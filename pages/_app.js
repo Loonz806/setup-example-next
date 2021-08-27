17f1d4cfce24d5bf5c6865ef4e1c0ef87eec7580
Lenny Peters <lpeters3@lululemon.com>
2021-08-27 09:17:16 -0700

    feat: 🎸 loader added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Router from "next/router";
 import React from "react";
 import PropTypes from "prop-types";
+import NProgress from "nprogress";
 // Some global styles but then afterward css module pattern instead
 import "../src/styles/globals.scss";
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
