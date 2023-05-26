433b2b6e6fdb6144c409996c7b498f6d8f8c0130
Lenny Peters <lpeters3@lululemon.com>
2023-05-26 12:32:18 -0700

    fix: 🐛 addressing bits

--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,31 @@ const HomePage = ({ posts }) => {
         <title>Setup Example Next | Lenny Peters</title>
       </Head>
       <main>
-        <h1>
+        {/* <h1>
           {posts.filter((blog) => blog.title === "My first blog post")[0]
             .title || "No title"}
-        </h1>
+        </h1> */}
         <ExampleComponent />
       </main>
     </>
   );
 };
 
-export async function getStaticProps() {
-  try {
-    const res = await fetch("http://localhost:3000/posts");
-    const posts = await res.json();
-    return {
-      props: {
-        posts,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {},
-    };
-  }
-}
+// export async function getStaticProps() {
+//   try {
+//     const res = await fetch("http://localhost:3000/posts");
+//     const posts = await res.json();
+//     return {
+//       props: {
+//         posts,
+//       },
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     return {
+//       props: {},
+//     };
+//   }
+// }
 
 export default HomePage;
