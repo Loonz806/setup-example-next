4c49830a2e1788bac027d2891e764a663b4a412e
Lenny Peters <lpeters3@lululemon.com>
2023-05-27 11:08:11 -0700

    fix: 🐛 more package.json updates

--- src/components/ExampleComponent/ExampleComponent.test.js
+++ src/components/ExampleComponent/ExampleComponent.test.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { render, act } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import ExampleComponent from "./ExampleComponent";
 
 describe("Example Component", () => {
-  test("render as expected", () => {
-    act(() => {
-      const { getByText } = render(<ExampleComponent />);
-      // screen.debug() don't forget to import screen
-      expect(getByText(`I am an example component`)).toBeInTheDocument();
-    });
+  it("should render as expected", () => {
+    const { getByText } = render(<ExampleComponent />);
+    // screen.debug() don't forget to import screen
+    expect(getByText(`I am an example component`)).toBeInTheDocument();
   });
 });
