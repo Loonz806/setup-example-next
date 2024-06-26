848ce9d76a8e55993402e780b6d2008035d1d410
Lenny Peters <lpeters3@lululemon.com>
2024-06-26 14:35:18 -0700

    fix: 🐛 addressing linting

--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -3,28 +3,28 @@ import Link from "next/link";
 import styles from "./PostList.module.scss";
 
 const PostsLists = ({ posts }) => {
-    const renderPosts = () => {
-      try {
-        return posts?.map((post) => {
-          const { title, author, date, content, id } = post;
-          return (
-            <article className={styles.saleItem} key={id}>
-              <Link href={`/blog/${encodeURIComponent(post.id)}`}>
-                <h3>{title}</h3>
-                <small>
-                  {author} | {date}
-                </small>
-                <hr />
-                <p>{content}</p>
-              </Link>
-            </article>
-          );
-        });
-      } catch (e) {
-        throw new Error(e, "new error");
-      }
-    };
-    return <div className={styles.postList}>{renderPosts()}</div>;
+  const renderPosts = () => {
+    try {
+      return posts?.map((post) => {
+        const { title, author, date, content, id } = post;
+        return (
+          <article className={styles.saleItem} key={id}>
+            <Link href={`/blog/${encodeURIComponent(post.id)}`}>
+              <h3>{title}</h3>
+              <small>
+                {author} | {date}
+              </small>
+              <hr />
+              <p>{content}</p>
+            </Link>
+          </article>
+        );
+      });
+    } catch (e) {
+      throw new Error(e, "new error");
+    }
+  };
+  return <div className={styles.postList}>{renderPosts()}</div>;
 };
 
 export default PostsLists;
